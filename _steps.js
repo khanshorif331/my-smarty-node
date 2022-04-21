9b7a7a7466c273c5afe50e0f89ea5863df6bd486
khanshorif331 <khanshorif331@gmail.com>
2022-04-22 05:49:45 +0600

    added steps

---
+++ _steps.js
@@ -0,0 +1,39 @@
+/*
+---------------------------
+basic server setup
+---------------------------
+--
+1. create a folder (manually or mkdir)
+2. open command line to that folder
+3. npm init
+4.npm install express
+5.create index.js file
+----------------------------
+
+
+
+-----------------------------
+for express/node server setup
+-----------------------------
+1.require express 
+2.create app variable
+3.declare port 
+4.set app.get('/')
+5.listen to port 
+6.nodemon index.js
+7.check your browser for that port
+
+------------------------------
+get method
+-------------------------------
+1.cord middleware
+
+
+-------------------------------
+
+-------------------------------
+post method
+-------------------------------
+1.
+
+*/
